136b18dd63124101eb36c0caed7c24675144fa68
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-08-10 22:42:57 +0000

    Update bank-account.cpp

--- CSM-158_C03/bank-account.cpp
+++ CSM-158_C03/bank-account.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class BankAccount {
 private:
     string name;
     int accountNumber;
-    string accountType; // checking/saving
+    string accountType;
     double balance;
-    double interestRate; // decimal
+    double interestRate;
     static int nextAccountNumber;
 
 public:
@@ -42,7 +41,7 @@ public:
         if (amount <= balance) {
             balance -= amount;
         } else {
-            cout << "Insufficient funds.\n";
+            cout << "Insufficient funds in account " << accountNumber << endl;
         }
     }
 
@@ -50,42 +49,47 @@ public:
         balance += balance * interestRate;
     }
 
-    // Getters
-    string getName() const { return name; }
-    int getAccountNumber() const { return accountNumber; }
-    string getAccountType() const { return accountType; }
-    double getBalance() const { return balance; }
-    double getInterestRate() const { return interestRate; }
+    void printAccount() const {
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Name: " << name << endl;
+        cout << "Type: " << accountType << endl;
+        cout << "Balance: $" << balance << endl;
+        cout << "Interest Rate: " << interestRate * 100 << "%" << endl;
+        cout << "-----------------------" << endl;
+    }
 };
 
-// Initialize static member
 int BankAccount::nextAccountNumber = 1000;
 
 int main() {
-    BankAccount accounts[10];
-
-    accounts[0].setAccountDetails("Pasca", "Saving", 5000, 0.02);
-    accounts[1].setAccountDetails("0x1BA88FF", "Checking", 2000, 0.01);
-
+    BankAccount accounts[10] = {
+        {"Pasca", "Saving", 5000, 0.02},
+        {"kevin", "Checking", 2000, 0.01},
+        {"Benjamin", "Saving", 3000, 0.03},
+        {"Ama", "Checking", 1500, 0.015},
+        {"Saira", "Saving", 8000, 0.025},
+        {"Eve", "Checking", 1200, 0.01},
+        {"Ado", "Saving", 7000, 0.02},
+        {"Yoasobi", "Checking", 2500, 0.015},
+        {"kenshi Yonezu", "Saving", 6000, 0.02},
+        {"ACAne", "Checking", 4000, 0.01}
+    };
+    
     accounts[0].deposit(1000);
     accounts[1].withdraw(500);
-    accounts[0].applyInterest();
+    accounts[2].applyInterest();
+    accounts[3].deposit(200);
+    accounts[4].withdraw(1000);
+    accounts[5].applyInterest();
+    accounts[6].deposit(3000);
+    accounts[7].withdraw(2000);
+    accounts[8].applyInterest();
+    accounts[9].deposit(700);
 
-    // First account
-    cout << "Account Number: " << accounts[0].getAccountNumber() << endl;
-    cout << "Name: " << accounts[0].getName() << endl;
-    cout << "Type: " << accounts[0].getAccountType() << endl;
-    cout << "Balance: $" << accounts[0].getBalance() << endl;
-    cout << "Interest Rate: " << accounts[0].getInterestRate() * 100 << "%" << endl;
-
-    cout << endl;
-
-    // Second account
-    cout << "Account Number: " << accounts[1].getAccountNumber() << endl;
-    cout << "Name: " << accounts[1].getName() << endl;
-    cout << "Type: " << accounts[1].getAccountType() << endl;
-    cout << "Balance: $" << accounts[1].getBalance() << endl;
-    cout << "Interest Rate: " << accounts[1].getInterestRate() * 100 << "%" << endl;
+    // Print all accounts
+    for (int i = 0; i < 10; i++) {
+        accounts[i].printAccount();
+    }
 
     return 0;
 }
